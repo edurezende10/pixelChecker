acfdbf9de5599b698bde46c641b5bef1e7fe9fad
unknown <edu_rezende11@hotmail.com>
2022-10-20 12:05:07 -0300

    criando servidor com express

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const puppeteer = require('puppeteer');
-const tikTokValidation=require('./tiktokValidation');
+const tikTokValidation = require('./tiktokValidation');
+let express = require('express')
+var cors = require('cors')
+
+let app = express()
+let url
+app.use(cors())
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.post('/', function (req, res, next) {
+  url = req.body.url
+  //res.json(req.body.url)
+  runScript(url)
+  res.send('ok')
+
+})
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+app.listen(3000)
+
+
+
 
 
 const scriptUrlPatterns = [
   '*'
 ]
-
-const requestCache = new Map();
 
 async function interceptRequestsForPage(page) {
   const client = await page.target().createCDPSession();
@@ -22,28 +43,21 @@ async function interceptRequestsForPage(page) {
     if (request.url == 'https://analytics.tiktok.com/api/v2/pixel') {
       let eventName = await JSON.parse(request.postData).event
       let eventParams = await JSON.parse(request.postData).properties
-      tikTokValidation(eventParams,eventName)
-   
+      tikTokValidation(eventParams, eventName)
+
     }
   });
 }
 
-(async function main() {
+
+const runScript = async function (url) {
   const browser = await puppeteer.launch({
     headless: false,
     //headless: 'chrome',
     defaultViewport: null,
     devtools: true,
-   
-
   });
-
   const page = (await browser.pages())[0];
   await page.goto('https://lcgbr-ecom.vercel.app/');
-
   await interceptRequestsForPage(page);
-
-
-
-})()
-
+}
